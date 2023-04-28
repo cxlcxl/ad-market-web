a2f4bcfd35eb15fad9d0e478a45bd911cf7f19b8
silent <2063096712@qq.com>
2023-04-28 21:48:42 +0800

    s

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import 'vant/lib/index.css';
+import { createRouter,createWebHashHistory} from "vue-router";
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: []
+})
 
 const app = createApp(App)
-app.mount('#app')
+app.use(router).mount('#app')
---
+++ src/settings.js
@@ -0,0 +1,7 @@
+const settings = {
+  RedirectUri: "https://market.skykj.com.cn/#/?sn=",
+  AppId: "wx2556f9550e2b8522",
+  XcxPath: "pages/friend/friend.html?mobile=",
+}
+
+export default settings
